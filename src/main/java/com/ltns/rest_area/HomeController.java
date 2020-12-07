ff205e6c1a51c4b020428e277972bbee203d0914
박성언 <tjddjs90@yahoo.com>
2020-12-08 01:21:03 +0900

    test delete

--- src/main/java/com/ltns/rest_area/HomeController.java
+++ src/main/java/com/ltns/rest_area/HomeController.java
@@ -4,52 +4,36 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.ltns.rest_area.domain.test.Test1;
-import com.ltns.rest_area.domain.test.Test2;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
-	
-	@Autowired
-	@Qualifier("sqlSessionTemplate")
-	SqlSession ss; 
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
 
-		int test1 =	ss.getMapper(Test1.class).test("s");
-		int test2 =	ss.getMapper(Test2.class).test("s");
-		
-		System.out.println("mapping error : " + test1);
-		System.out.println("mapping error : " + test2);
-		
+		String formattedDate = dateFormat.format(date);
+
+		model.addAttribute("serverTime", formattedDate);
+
 		return "home";
 	}
-	
+
 }
--- src/main/java/com/ltns/rest_area/domain/test/Test2.java
+++
@@ -1,7 +0,0 @@
-package com.ltns.rest_area.domain.test;
-
-import com.ltns.rest_area.domain.DAO;
-
-public interface Test2 extends DAO{
-
-}
