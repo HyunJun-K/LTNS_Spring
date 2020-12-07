d1560d8e2f9a7c98fc05e7778a1422fc47379a9a
홍성혁 <markhong93@naver.com>
2020-12-07 16:42:31 +0900

    수정

--- ERD/user/DCL.sql
+++ ERD/user/DCL.sql
@@ -1,6 +1,6 @@
 -- 테이블 스페이스 생성
 -- 크기는 10MB 로 하며, 용량이 꽉찼을씨 2MB의 크기로 자동으로 증가합니다.
-CREATE TABLESPACE LTNSjsp
+CREATE TABLESPACE LTNSspring
 DATAFILE 'C:\ProjectWork\LTNS2\OracleDB\LTNSspring.dbf'
 SIZE 10M AUTOEXTEND ON NEXT 2M MAXSIZE UNLIMITED
 PERMANENT
